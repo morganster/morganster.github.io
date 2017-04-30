970cb9e4d19eefb27548afa978937713bfe05940
Carlos E. Jaramillo L <granmorgan@gmail.com>
2017-04-29 20:23:50 -0500

    start config

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    EducationComponent,
+    ExperienceComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
