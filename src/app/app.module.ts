c0c31e9e8fbcddcfbbdd8c766c637ade099ff77f
Carlos E. Jaramillo L <granmorgan@gmail.com>
2017-07-20 08:24:31 -0500

    add som styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { StickyModule } from 'ng2-sticky-kit/ng2-sticky-kit';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { EducationComponent } from './education/education.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'education',      component: EducationComponent },
+  { path: 'portfolio',      component: PortfolioComponent },
+  { path: 'contact',      component: ContactComponent },
+  {
+    path: 'experience',
+    component: ExperienceComponent,
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +35,15 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
     HomeComponent,
     EducationComponent,
     ExperienceComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    PageNotFoundComponent,
+    ContactComponent,
   ],
   imports: [
+     RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     FormsModule,
     HttpModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.less']
+  styleUrls: ['./home.component.less'],
 })
 export class HomeComponent implements OnInit {
 
