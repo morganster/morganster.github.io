7b54621c3f8880f0076bcd63a13242d86e776c36
Carlos Eduardo Jaramillo Londoño <cejl397@gmail.com>
2017-10-01 10:25:15 -0500

    change skills view for svg icons

--- src/skills/skills.vue
+++ src/skills/skills.vue
@@ -7,12 +7,12 @@
       </div>
     </div>
     <div class="row skills">
-      <div class="col-12 col-sm-12 col-md-12 col-lg-6 skill-col">
-        <chartjs-horizontal-bar :data="myData" :datalabel="mylabel" :labels="mylabels" :bordercolor="myBackgroundColor" :backgroundcolor="myBackgroundColor" :option="myOption" ></chartjs-horizontal-bar>
+      <div class="col-xs-4 col-md-2" v-for="skill in mySkills">
+        <div class="icon">
+            <img class="img-fluid" v-bind:src="skill.url" v-bind:alt="skill.name">
+        </div>
       </div>
-      <div class="col-12 col-sm-12 col-md-12 col-lg-6 skill-col">
-            <chartjs-horizontal-bar :data="mydataSecond" :datalabel="mylabel" :option="myOption" :labels="mylabelsSecond" :bordercolor="myBackgroundColorSecond" :backgroundcolor="myBackgroundColorSecond" ></chartjs-horizontal-bar>
-      </div>
+      
     </div>
   </div>
 </template>
@@ -20,51 +20,68 @@
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+  export interface ISkill {
+    url: string;
+    name: string;
+  };
 
 @Component({
 
 })
 export default class Skills extends Vue {
+  mySkills:ISkill[] = [
+    {
+      url: "../assets/angular-icon.svg",
+      name: "Angular",
+    },
+    {
+      url: "../assets/vue.svg",
+      name: "VueJS",
+    },
+    {
+      url: "../assets/nodejs-icon.svg",
+      name: "NodeJS",
+    },
+    {
+      url: "../assets/codeigniter.svg",
+      name: "Codeigniter",
+    },
+    {
+      url: "../assets/jquery.svg",
+      name: "JQuery",
+    },
+    {
+      url: "../assets/javascript.svg",
+      name: "Javascript",
+    },
+    {
+      url: "../assets/git-icon.svg",
+      name: "Git",
+    },
+    {
+      url: "../assets/css-3.svg",
+      name: "CSS3",
+    },
+    {
+      url: "../assets/html-5.svg",
+      name: "HTML5",
+    },
+    {
+      url: "../assets/typescript-icon.svg",
+      name: "TypeScript",
+    },
+    {
+      url: "../assets/mysql.svg",
+      name: "MySQL",
+    },
+    {
+      url: "../assets/php.svg",
+      name: "PHP",
+    },
+
+  ];
   mylabel: string = 'Skills';
-  mylabels: string[] = ['Angularjs', 'Angular', 'Vuejs', 'html', 'css', 'javascript', 'typescript'];
-  mylabelsSecond: string[] = ['mysql', 'php', 'photoshop', 'nodejs', 'express', 'webpack', 'linux'];
-  myColors: string[] = ["fafafa","cacaca"];
-  myData: any[] = [90, 90, 70, 90, 95, 90, 80];
-  /*myData: any[] = [{label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 20]}];*/
-  myBackgroundColor: string[] = ["#a6120d","#dd0031", "#41b883","#f47933","#008fd5","#faab4b","#0074c1"];
-  myBackgroundColorSecond: string[] = ["#017495","#777bb4", "#35c1e6","#90c53f","#707070","#7d9ede","#010101"];
-  mydataSecond: number[] = [90, 90, 76, 70, 65, 80, 85];
-  myOption = {
-     legend: {
-            display: false,
-            },
-    responsive: true,
-    maintainAspectRatio: true,
-    label: {
-      display: false,
-    },
-
-    scales: {
-      xAxes: [{
-        gridLines: {
-                    display:false
-                },
-        display: true,
-        ticks: {
-          beginAtZero: true,
-        },
-      }],
-      yAxes: [{
-            gridLines: {
-                display: false
-
-            }
-        }]
-    },
-  }
-
+  
   // lifecycle hook
   mounted() {
   }
@@ -72,6 +89,9 @@ export default class Skills extends Vue {
 Vue.component('skills', Skills)
 </script>
 <style>
+.icon{
+  padding: 2em;
+}
 .skills {
    padding: 2em;
   background-color: #fff;
